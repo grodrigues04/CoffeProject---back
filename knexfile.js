7bc639b7226ffe004d0ae8ab914989865e317023
grodrigues04 <gurodrigues.b@gmail.com>
2024-12-05 13:36:40 -0300

    feat: add environment variables

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
     development: {
         client: 'pg',
         connection: {
-            host: process.env.DB_HOST || '127.0.0.1', 
-            user: process.env.DB_USER || 'postgres',
-            password: process.env.DB_PASS || '123',
-            database: process.env.DB_NAME || 'BalleCofe',
+            host: process.env.DB_HOST, 
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
         },
     },
 };
