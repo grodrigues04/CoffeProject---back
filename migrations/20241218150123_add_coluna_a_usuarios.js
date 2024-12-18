bee58dcb48220b4f11bd87298c5e5c9e0efeb65d
grodrigues04 <gurodrigues.b@gmail.com>
2024-12-18 12:04:55 -0300

    feat: add id_carrinho to users database

---
+++ migrations/20241218150123_add_coluna_a_usuarios.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('usuarios', function(table) {
+        table.integer('id_carrinho').unique();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('usuarios', function(table) {
+        table.dropColumn('id_carrinho');
+    });
+};
