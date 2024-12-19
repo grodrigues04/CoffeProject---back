270893d947dcdca26329ac5024242cf24ab4bb73
grodrigues04 <gurodrigues.b@gmail.com>
2024-12-18 21:27:47 -0300

    feat: add carrinhoroutes on index.js

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,13 @@
 const userRoutes = require('./userRoutes');
 const coffeRoutes = require('./coffeRoutes');
 const carrinhoRoutes = require('./carrinhoRoutes');
+const pedidosRoutes = require('./pedidosRoutes');
 //Arquivo que centraliza todas as rotas.
 const allRoutes = [
   ...userRoutes,
   ...coffeRoutes,
-  ...carrinhoRoutes
+  ...carrinhoRoutes,
+  ...pedidosRoutes
 ];
 
 module.exports = allRoutes;
